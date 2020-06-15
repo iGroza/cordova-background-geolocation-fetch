2a74274e4341a14abf36167ab8e8d89981e9c65a
iGroza1 <xneonxneon@mail.ru>
2020-06-16 00:50:45 +0700

    added objective-c header

---
+++ src/ios/CDVBackgroundGeolocationFetch-Swift.h
@@ -0,0 +1,15 @@
+//
+//  CDVBackgroundGeolocationFetch-Swift.h
+//
+//  Created by iGroza on 08.06.2020.
+//  Copyright © 2020 iGroza. All rights reserved.
+//
+
+@class CDVBackgroundGeolocationFetch;
+
+@interface CDVBackgroundGeolocationFetch : CDVPlugin
+-(void)ready:(CDVInvokedUrlCommand*)command;
+-(void)startTracking:(CDVInvokedUrlCommand*)command;
+-(void)stopTracking:(CDVInvokedUrlCommand*)command;
+-(void)postCurrentLocation:(CDVInvokedUrlCommand*)command;
+@end
